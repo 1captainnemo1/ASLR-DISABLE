3f94f209e2aef9a1c2d1d9faab80392c1c252bb3
1captainnemo1 <37461247+1captainnemo1@users.noreply.github.com>
2019-11-07 01:32:15 +0530

    Update DisableASLR.bat

--- DisableASLR.bat
+++ DisableASLR.bat
@@ -1,14 +1,15 @@
 @ECHO OFF
 SETLOCAL
 
-ECHO ASLR Enable / Diable Batch Script - Please run as admin
+ECHO ASLR Enable or Diable Bat script 
+ECHO Run as Administrator
 ECHO Once Disabled , binaries will be prone to Stack smashing and buffer overflow exploits
 ECHO Regards Captain_Nemo
  
-set /p Choice=Want to Enable or Disable ASLR? (e or d):%=%
+set /p Choice=Want to Enable or Disable ASLR? (en or di):%=%
 
-if "%Choice%"=="e" goto :ENABLE
-if "%Choice%"=="d" goto :DISABLE
+if "%Choice%"=="en" goto :ENABLE
+if "%Choice%"=="di" goto :DISABLE
 
 :ENABLE
 	reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "MoveImages" /t REG_DWORD /d 1 /f
